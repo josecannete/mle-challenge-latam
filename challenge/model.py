486087c9785723742750c12d3c7ffcfacefde1a9
José Cañete <josecannete@gmail.com>
2025-01-22 18:04:26 -0300

    partially working model, without class weights for now

--- challenge/model.py
+++ challenge/model.py
@@ -2,14 +2,46 @@ import pandas as pd
 
 from typing import Tuple, Union, List
 
+from datetime import datetime
+
+from sklearn.linear_model import LogisticRegression
+
+
+def get_minutes_diff(data: pd.DataFrame) -> int:
+    fecha_o = datetime.strptime(data["Fecha-O"], "%Y-%m-%d %H:%M:%S")
+    fecha_i = datetime.strptime(data["Fecha-I"], "%Y-%m-%d %H:%M:%S")
+    minutes_diff = ((fecha_o - fecha_i).total_seconds()) / 60
+    return minutes_diff
+
 
 class DelayModel:
-    def __init__(self):
-        self._model = None  # Model should be saved in this attribute.
+    _FEATURE_COLS: list[str] = [
+        "OPERA_Latin American Wings",
+        "MES_7",
+        "MES_10",
+        "OPERA_Grupo LATAM",
+        "MES_12",
+        "TIPOVUELO_I",
+        "MES_4",
+        "MES_11",
+        "OPERA_Sky Airline",
+        "OPERA_Copa Air",
+    ]
+    _DELAY_THRESHOLD: int = 15
 
-    def preprocess(self, data: pd.DataFrame, target_column: str = None) -> Union(
-        Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame
-    ):
+    def __init__(self):
+        self._model = LogisticRegression()
+
+    def _add_delay_column(self, data: pd.DataFrame) -> pd.DataFrame:
+        data["delay"] = data.apply(lambda x: get_minutes_diff(x), axis=1)
+        data["delay"] = data["delay"].apply(
+            lambda x: 1 if x > self._DELAY_THRESHOLD else 0
+        )
+        return data
+
+    def preprocess(
+        self, data: pd.DataFrame, target_column: str = None
+    ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
         """
         Prepare raw data for training or predict.
 
@@ -22,7 +54,23 @@ class DelayModel:
             or
             pd.DataFrame: features.
         """
-        return
+
+        features = pd.concat(
+            [
+                pd.get_dummies(data["OPERA"], prefix="OPERA"),
+                pd.get_dummies(data["TIPOVUELO"], prefix="TIPOVUELO"),
+                pd.get_dummies(data["MES"], prefix="MES"),
+            ],
+            axis=1,
+        )
+
+        selected_features = features[self._FEATURE_COLS]
+
+        if target_column:
+            target = self._add_delay_column(data)
+            return selected_features, target[[target_column]]
+
+        return selected_features
 
     def fit(self, features: pd.DataFrame, target: pd.DataFrame) -> None:
         """
@@ -32,6 +80,8 @@ class DelayModel:
             features (pd.DataFrame): preprocessed data.
             target (pd.DataFrame): target.
         """
+        # TODO: incorporate class weights
+        self._model.fit(features, target)
         return
 
     def predict(self, features: pd.DataFrame) -> List[int]:
@@ -44,4 +94,4 @@ class DelayModel:
         Returns:
             (List[int]): predicted targets.
         """
-        return
+        return self._model.predict(features)
