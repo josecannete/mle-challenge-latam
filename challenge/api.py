0956dd755f357e0474c417b0115f9763054bb40d
José Cañete <josecannete@gmail.com>
2025-01-22 23:47:37 -0300

    wip: adding api, still not working on tests

--- challenge/api.py
+++ challenge/api.py
@@ -1,6 +1,35 @@
+from contextlib import asynccontextmanager
 import fastapi
+import pandas as pd
+from pydantic import BaseModel
 
-app = fastapi.FastAPI()
+from challenge.model import DelayModel
+
+delay_model = None
+
+
+@asynccontextmanager
+async def lifespan(app: fastapi.FastAPI):
+    global delay_model
+    # Load the ML model
+    delay_model = DelayModel()
+    delay_model.load_model("trained_model.joblib")
+    yield
+    # Clean up the ML models and release the resources
+    delay_model = None
+
+
+app = fastapi.FastAPI(lifespan=lifespan)
+
+
+class Flight(BaseModel):
+    OPERA: str
+    TIPOVUELO: str
+    MES: int
+
+
+class BatchPredictionQuery(BaseModel):
+    flights: list[Flight]
 
 
 @app.get("/health", status_code=200)
@@ -9,5 +38,8 @@ async def get_health() -> dict:
 
 
 @app.post("/predict", status_code=200)
-async def post_predict() -> dict:
-    return
+async def post_predict(query: BatchPredictionQuery) -> dict:
+    data_df = pd.DataFrame.from_records([flight.dict() for flight in query.flights])
+    features = delay_model.preprocess(data_df)
+    predictions = delay_model.predict(features)
+    return {"predict": predictions}
