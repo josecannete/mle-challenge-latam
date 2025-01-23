40ca34a7033ca64f5c2a0192440bd9d5bb15b902
José Cañete <josecannete@gmail.com>
2025-01-23 09:35:21 -0300

    making the validations for each field, also changing the default error code of invalid request from 422 to 400

--- challenge/api.py
+++ challenge/api.py
@@ -1,9 +1,13 @@
 from contextlib import asynccontextmanager
 import fastapi
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 import pandas as pd
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from challenge.model import DelayModel
+from challenge.constants import _VALID_OPERA_VALUES
 
 delay_model = None
 
@@ -22,10 +26,20 @@ async def lifespan(app: fastapi.FastAPI):
 app = fastapi.FastAPI(lifespan=lifespan)
 
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+    request: fastapi.Request, exc: RequestValidationError
+):
+    return JSONResponse(
+        status_code=fastapi.status.HTTP_400_BAD_REQUEST,
+        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+    )
+
+
 class Flight(BaseModel):
-    OPERA: str
-    TIPOVUELO: str
-    MES: int
+    OPERA: str = Field(pattern=r"|".join(_VALID_OPERA_VALUES))
+    TIPOVUELO: str = Field(pattern=r"N|I")
+    MES: int = Field(ge=1, le=12)
 
 
 class BatchPredictionQuery(BaseModel):
@@ -39,7 +53,9 @@ async def get_health() -> dict:
 
 @app.post("/predict", status_code=200)
 async def post_predict(query: BatchPredictionQuery) -> dict:
-    data_df = pd.DataFrame.from_records([flight.dict() for flight in query.flights])
+    data_df = pd.DataFrame.from_records(
+        [flight.model_dump() for flight in query.flights]
+    )
     features = delay_model.preprocess(data_df)
     predictions = delay_model.predict(features)
     return {"predict": predictions}
